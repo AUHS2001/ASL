09e8c7764484b87059e282157dafccbaf75b3204
Muhammad <danish.dev000@gmail.com>
2024-01-21 14:20:31 +0500

    Add Meta content security policy

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body className={inter.className}>
         <ToastContainer
           position="top-right"
