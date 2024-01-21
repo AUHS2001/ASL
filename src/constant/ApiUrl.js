60810aac81cd0505d29bec010fb084c79c50c2b0
Muhammad <danish.dev000@gmail.com>
2024-01-21 14:26:21 +0500

    API URL

--- src/constant/ApiUrl.js
+++ src/constant/ApiUrl.js
@@ -1,2 +1,3 @@
 // export const API_URL="http://127.0.0.1:8000"
+// export const API_URL="http://ai-sign-env.eba-z9pwwi3e.us-east-1.elasticbeanstalk.com"
 export const API_URL="http://ai-sign-env.eba-z9pwwi3e.us-east-1.elasticbeanstalk.com"
