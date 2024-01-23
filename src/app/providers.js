944c484ef425ae0c110081c6def332301de8a2d7
danish-symits <muhammaddanish.symits@gmail.com>
2024-01-23 20:22:55 +0500

    add redux toolkit

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,6 +1,8 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import ThemeRegistry from "@/Theme/ThemeRegistry";
 import { AuthContextProvider } from "@/context/AuthContext";
+import { Provider } from 'react-redux'
+
 
 export function Providers({ children }) {
   return (
